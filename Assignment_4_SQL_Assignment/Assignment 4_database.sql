fb53f60f3ebe1dfd0f9f4699b97cb162e99d16d4
Derek Nguyen <dereknguyen@Dereks-MacBook-Pro.local>
2019-10-22 14:16:51 -0700

    create database tables

--- Assignment_4_SQL_Assignment/Assignment 4_database.sql
+++ Assignment_4_SQL_Assignment/Assignment 4_database.sql
@@ -6,11 +6,21 @@ CREATE TABLE IF NOT EXISTS exhibits (
     name VARCHAR(20) NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
-    curator_id INT 
+	
+    curator_id INT,
+    CONSTRAINT FK_CuratorExhibit FOREIGN KEY (curator_id) REFERENCES Curators(id)
     
 );
 
+CREATE TABLE IF NOT EXISTS curators (
+	id INT NOT NULL,
+    PRIMARY KEY (id),
+    name VARCHAR(20) NOT NULL,
+    bio TEXT
+);
 
 DROP TABLE exhibits;
+DROP TABLE curators;
 
 describe exhibits;
+describe curators;
